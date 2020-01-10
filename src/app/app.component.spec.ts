238eb960264d4d78d19647a1edd8c198fcad2d20
Ogunwobi Philip <paogunwobi@gmail.com>
2020-01-10 10:12:13 +0100

    test should fail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -19,7 +20,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ng-azure-devops'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-azure-devops');
+    expect(app.title).toEqual('ng-azure-devopssss');
   });
 
   it('should render title', () => {
