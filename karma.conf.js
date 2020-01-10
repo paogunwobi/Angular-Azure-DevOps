3e96810509339563d423fc0649b61842cf5f1d12
Ogunwobi Philip <paogunwobi@gmail.com>
2020-01-10 09:59:13 +0100

    Setup update commit

--- karma.conf.js
+++ karma.conf.js
@@ -4,29 +4,30 @@ const process = require('process');
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage-istanbul-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
+      require("karma-junit-reporter")
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-azure-devops'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require("path").join(__dirname, "./coverage/ng-azure-devops"),
+      reports: ["html", "lcovonly", "text-summary"],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ["progress", "kjhtml", "junit"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ["ChromeHeadless"],
     singleRun: false,
     restartOnFileChange: true
   });
